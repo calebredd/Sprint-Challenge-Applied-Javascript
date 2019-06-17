874fae459204e962dfb2254ed2206e4c7292f022
calebredd <caleb.redd@yahoo.com>
2019-06-16 20:36:47 -0700

    Added carousel functionality to top of webpage with 4 photos

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,43 @@
 class Carousel {
-
+  constructor(image) {
+    this.image = image;
+    this.order = this.image.dataset.order;
+  }
 }
+document
+  .querySelector(".left-button")
+  .addEventListener("click", function left() {
+    let e = carousel.querySelector(".show").dataset.order;
+    //console.log(e);
+    carouselImages.forEach(image => image.classList.remove("show"));
+    let select = e - 1;
+    if (select < 1) {
+      select = i;
+    } else if (select > i) {
+      select = 1;
+    }
+    carousel.querySelector(`img[data-order="${select}"]`).classList.add("show");
+  });
+document
+  .querySelector(".right-button")
+  .addEventListener("click", function right() {
+    let e = Number(carousel.querySelector(".show").dataset.order);
+    carouselImages.forEach(image => image.classList.remove("show"));
+    let select = e+1;
+    if (select < 1) {
+      select = i;
+    } else if (select > i) {
+      select = 1;
+    }
+    carousel.querySelector(`img[data-order="${select}"]`).classList.add("show");
+  });
 
-let carousel = document.querySelector();
-
+let carousel = document.querySelector(".carousel");
+let carouselImages = carousel.querySelectorAll("img");
+let i = carouselImages.length;
+carouselImages.forEach(image => {
+  new Carousel(image);
+});
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
