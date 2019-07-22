8aeeb2ff0aa3fdb9116f1b8acc03a0a030291c3c
calebredd <caleb.redd@yahoo.com>
2019-07-21 19:55:48 -0700

    Rebuilt carousel using javascript, added dataset.order for looping through pictures

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,3 +1,39 @@
+var carouselDiv = document.createElement("div");
+let carousel = document.querySelector(".carousel-container");
+carouselDiv.classList.add("carousel");
+carousel.appendChild(carouselDiv);
+var left = document.createElement("div");
+left.classList.add("left-button");
+carouselDiv.appendChild(left);
+left.textContent="<";
+
+var img1 = document.createElement("img");
+img1.classList.add("show");
+img1.dataset["order"] = 1;
+img1.src = "/assets/carousel/mountains.jpeg";
+carouselDiv.appendChild(img1);
+
+var img2 = document.createElement("img");
+img2.dataset["order"] = 2;
+img2.src = "/assets/carousel/computer.jpeg";
+carouselDiv.appendChild(img2);
+
+var img3 = document.createElement("img");
+img3.dataset["order"] = 3;
+img3.src = "/assets/carousel/trees.jpeg";
+carouselDiv.appendChild(img3);
+
+var img4 = document.createElement("img");
+img4.dataset["order"] = 4;
+img4.src = "/assets/carousel/turntable.jpeg";
+carouselDiv.appendChild(img4);
+
+var right = document.createElement("div");
+right.classList.add("right-button");
+carouselDiv.appendChild(right);
+right.textContent=">";
+
+
 class Carousel {
   constructor(image) {
     this.image = image;
@@ -28,7 +64,6 @@ document.querySelector(".right-button").addEventListener("click", () => {
   carousel.querySelector(`img[data-order="${select}"]`).classList.add("show");
 });
 
-let carousel = document.querySelector(".carousel-container");
 let carouselImages = carousel.querySelectorAll("img");
 let i = carouselImages.length;
 carouselImages.forEach(image => {
@@ -46,11 +81,11 @@ carouselImages.forEach(image => {
 
 /* HTML:
   <div class="carousel">
-    <div class="left-button"> < </div>
-    <img src="./assets/carousel/mountains.jpeg" />
-    <img src="./assets/carousel/computer.jpeg" />
-    <img src="./assets/carousel/trees.jpeg" />
-    <img src="./assets/carousel/turntable.jpeg" />
-    <div class="right-button"> > </div>
+  <div class="left-button"> < </div>
+  <img src="./assets/carousel/mountains.jpeg" />
+  <img src="./assets/carousel/computer.jpeg" />
+  <img src="./assets/carousel/trees.jpeg" />
+  <img src="./assets/carousel/turntable.jpeg" />
+  <div class="right-button"> > </div>
   </div>
-*/
+  */
